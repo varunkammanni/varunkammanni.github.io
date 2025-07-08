a2a48b6d3bd09a8de4cf1ef1de27bd5a79e64a0f
Varun Kammanni <varunkv91@gmail.com>
2025-07-08 13:10:05 -0400

    Updated Style
    Changed UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const navLinks = document.querySelectorAll('nav a');
-    navLinks.forEach(link => {
-      link.addEventListener('click', (e) => {
-        e.preventDefault();
-        const targetId = link.getAttribute('href').startsWith('#') 
-          ? link.getAttribute('href').substring(1)
-          : link.getAttribute('href');
-        if (targetId.includes('.html')) {
-          window.location.href = targetId;
-        } else {
-          const targetSection = document.getElementById(targetId);
-          if (targetSection) {
-            targetSection.scrollIntoView({ behavior: 'smooth' });
-          }
+  const navLinks = document.querySelectorAll('nav a');
+  navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const targetId = link.getAttribute('href').startsWith('#') 
+        ? link.getAttribute('href').substring(1)
+        : link.getAttribute('href');
+      if (targetId.includes('.html') || targetId === '/') {
+        window.location.href = targetId === '/' ? '/' : targetId;
+      } else {
+        const targetSection = document.getElementById(targetId);
+        if (targetSection) {
+          targetSection.scrollIntoView({ behavior: 'smooth' });
         }
-      });
+      }
     });
   });
+});
